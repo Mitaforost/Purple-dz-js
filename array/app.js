90aec429219f0d3dc20949a596b4c77ba86bb8bb
Danila <zaharchuk.danila18@gmail.com>
2025-09-03 20:54:25 +0300

    fix: array v0.2

--- array/app.js
+++ array/app.js
@@ -1,20 +1,13 @@
 function crypto(password) {
-    const half = Math.floor(password.length / 2);
-
-    const left = password.slice(0, half).split('').reverse().join('');
-    const right = password.slice(half).split('').reverse().join('');
-
-    return left + right;
+    return password.split('').reverse().join('');
 }
+
 
 function check(encrypted, original) {
-    const half = Math.floor(encrypted.length / 2);
-
-    const left = encrypted.slice(0, half).split('').reverse().join('');
-    const right = encrypted.slice(half).split('').reverse().join('');
-
-    return (left + right) === original;
+    const decrypted = encrypted.split('').reverse().join('');
+    return decrypted === original;
 }
+
 
 console.log(crypto('password'));
 console.log(check('ssapdorw', 'password'));
